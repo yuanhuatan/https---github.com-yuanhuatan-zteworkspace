a58ced8d81a0d28781b7526f228d08aff7a33b35
yuanhuatan <titian@vip.qq.com>
2016-11-11 22:50:47 +0800

    test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+##test python script file
+print ('''begin python app!''')
+import sys
+for i in sys.argv:
+    print(i)
+
+print(sys.argv)
+
+worklist=[1,2,3]
+print(worklist)
+
+for i in sys.argv:
+    print(sys.argv)
+
+print ('end python app!')
+
